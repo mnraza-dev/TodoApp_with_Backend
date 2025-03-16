d96b5670fdc6a3d78eb15692f4b7599c19720b8b
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-16 23:08:37 +0530

    Add body-parser dependency for handling request bodies

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 dotenv.config();
-
 const app = express();
-
 const PORT = process.env.PORT || 3000
-
-
 
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`)
